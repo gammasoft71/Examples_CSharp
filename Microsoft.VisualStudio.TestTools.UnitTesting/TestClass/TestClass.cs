7122bc3b4943e59452173fc9eaf32ad24ea7716d
gammasoft71 <gammasoft71@gmail.com>
2018-06-27 23:17:15 +0200

    add UnitTesting examples"

---
+++ Microsoft.VisualStudio.TestTools.UnitTesting/TestClass/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests {
+  // The class DirectoryTest must be declared with TestClass attribute.
+  [TestClass]
+  public class DirectoryTest {
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext) {
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup() {
+    }
+
+    // This is the method that is called before any tests in a class test are run.
+    [TestInitialize]
+    public void TestInitialize() {
+      savedCurrentDirecory = Environment.CurrentDirectory;
+    }
+
+    // This is the method that is called after any tests in a class test are run.
+    [TestCleanup]
+    public void TestCleanup() {
+      Environment.CurrentDirectory = savedCurrentDirecory;
+    }
+
+    [TestMethod]
+    public void ChangeCurrentDirectoryWithDownloads() {
+      Environment.CurrentDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+    }
+
+    [TestMethod]
+    public void ChangeCurrentDirectoryWithPotatoes() {
+      Environment.CurrentDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Potatoes");
+    }
+
+    private string savedCurrentDirecory;
+  }
+}
--- System.Windows.Forms/ProgressBar2/ProgressBar2.cs
+++ System.Windows.Forms/ProgressBar2/ProgressBar2.cs
@@ -1,31 +1,22 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Examples {
   class Form1 : Form {
-    // The main entry point for the application.
     public static void Main() {
       Application.EnableVisualStyles();
-      Application.Run(new Form1 {Text = "ProgressBar2 Example" });
+      Application.Run(new Form1 {Text = "ProgressBar2 Example"});
     }
 
     public Form1() {
-      this.progressBar1 = new ProgressBar {Parent = this, Location = new System.Drawing.Point(50, 50), Style = ProgressBarStyle.Continuous, Width = 200};
-      this.progressBar2 = new ProgressBar {Parent = this, Location = new System.Drawing.Point(50, 80), Style = ProgressBarStyle.Continuous, Width = 200, Value = 50};
-      this.progressBar3 = new ProgressBar {Parent = this, Location = new System.Drawing.Point(50, 110), Style = ProgressBarStyle.Continuous, Width = 200, Maximum = 300, Value = 300};
-      this.progressBar4 = new ProgressBar {Parent = this, Location = new System.Drawing.Point(50, 140), Style = ProgressBarStyle.Continuous, Width = 200, Maximum = 140};
-      this.progressBar5 = new ProgressBar {Parent = this, Location = new System.Drawing.Point(50, 170), Style = ProgressBarStyle.Marquee, Width = 200};
-      this.timer = new Timer {Interval = 50, Enabled = true};
-      this.timer.Tick += delegate (object sender, EventArgs e) {
-        this.progressBar4.Value = this.progressBar4.Value < this.progressBar4.Maximum ? this.progressBar4.Value + 1 : this.progressBar4.Minimum;
-      };
+      this.Controls.Add(new ProgressBar {Location = new Point(50, 50), Style = ProgressBarStyle.Continuous, Width = 200});
+      this.Controls.Add(new ProgressBar {Location = new Point(50, 80), Style = ProgressBarStyle.Continuous, Width = 200, Value = 50});
+      this.Controls.Add(new ProgressBar {Location = new Point(50, 110), Style = ProgressBarStyle.Continuous, Width = 200, Maximum = 300, Value = 300});
+      this.Controls.Add(new ProgressBar {Location = new Point(50, 140), Style = ProgressBarStyle.Continuous, Width = 200, Maximum = 140});
+      this.Controls.Add(new ProgressBar {Location = new Point(50, 170), Style = ProgressBarStyle.Marquee, Width = 200});
+      Timer timer = new Timer {Interval = 50, Enabled = true};
+      timer.Tick += (sender, e) => (this.Controls[3] as ProgressBar).Value = (this.Controls[3] as ProgressBar).Value < (this.Controls[3] as ProgressBar).Maximum ? (this.Controls[3] as ProgressBar).Value + 1 : (this.Controls[3] as ProgressBar).Minimum;
     }
-
-    private ProgressBar progressBar1;
-    private ProgressBar progressBar2;
-    private ProgressBar progressBar3;
-    private ProgressBar progressBar4;
-    private ProgressBar progressBar5;
-    private Timer timer;
   }
 }
