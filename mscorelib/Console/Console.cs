ff936e71a53fcd896e3c1d5b8c0f54879eba8b71
gammasoft71 <gammasoft71@gmail.com>
2018-08-12 19:47:34 +0200

    Add Console

---
+++ mscorelib/Console/Console.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Examples {
+  class Program {
+    public static void Main(string[] args) {
+      Console.Write("Hello ");
+      Console.WriteLine("World!");
+      Console.Write("Enter your name: ");
+      string name = Console.ReadLine();
+      Console.Write("Good day, ");
+      Console.Write(name);
+      Console.WriteLine("!");
+    }
+  }
+}
+
+// This code produces the following output:
+//
+// Hello, World!
