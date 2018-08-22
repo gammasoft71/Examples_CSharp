51a8b58d6da95c2cf7f58b8903da067399c171d0
gammasoft71 <gammasoft71@gmail.com>
2018-08-22 13:02:50 +0200

    add TestConsole

---
+++ Tests/TestConsole/TestConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld {
+  class MainClass {
+    class Foo {
+      public void Print(string s) {
+        Console.WriteLine(s);
+      }
+    };
+    delegate void Print(string s);
+    public static void Main(string[] args) {
+      //Console.WriteLine("Hello World!");
+      Foo foo = new Foo();
+      Print print = foo.Print;
+
+      print += foo.Print;
+      print += foo.Print;
+
+      print("Hello, World!");
+    }
+  }
+}
+
+// This code produces the following output:
+//
+// Hello, World!
