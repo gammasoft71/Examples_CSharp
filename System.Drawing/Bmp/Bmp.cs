f9c9c61bbec41248baf93a9e01376874b8fff74b
gammasoft71 <gammasoft71@gmail.com>
2018-08-30 23:08:30 +0200

    add Bmp

---
+++ System.Drawing/Bmp/Bmp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace Examples {
+  class Program {
+    public static void Main(string[] args) {
+      Bitmap bitmap = new Bitmap(Examples.Properties.Resources.Gammasoft);
+
+      Console.WriteLine("HorizontalResolution = {0}", bitmap.HorizontalResolution);
+      Console.WriteLine("VerticalResolution = {0}", bitmap.VerticalResolution);
+      Console.WriteLine("PhysicalDimension = {0}", bitmap.PhysicalDimension);
+      Console.WriteLine("PixelFormat = {0}", bitmap.PixelFormat);
+      Console.WriteLine("RawFormat = {0}", bitmap.RawFormat);
+      Console.WriteLine("Flags = {0}", bitmap.Flags);
+      Console.WriteLine("Size = {0}", bitmap.Size);
+    }
+  }
+}
+
+// This code produces the following output:
+//
+// HorizontalResolution = 96
+// VerticalResolution = 96
+// PhysicalDimension = {Width=256, Height=256}
+// PixelFormat = Format32bppArgb
+// RawFormat = [ImageFormat: b96b3caa-0728-11d3-9d7b-0000f81ef32e]
+// Flags = 2
+// Size = {Width=256, Height=256}
