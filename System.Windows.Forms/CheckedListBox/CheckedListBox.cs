c6fbd3fe7e692376e25d391a3ac5a6f3210cede0
gammasoft71 <gammasoft71@gmail.com>
2018-08-30 23:09:17 +0200

    add CheckedListBox

---
+++ System.Windows.Forms/CheckedListBox/CheckedListBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examples {
+  class MainForm : Form {
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      this.Text = "CheckedListBox example";
+      this.StartPosition = FormStartPosition.Manual;
+      this.Location = new System.Drawing.Point(200, 200);
+      this.ClientSize = new System.Drawing.Size(200, 240);
+
+      this.checkedlistBox.Parent = this;
+      this.checkedlistBox.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom;
+      this.checkedlistBox.Bounds = new System.Drawing.Rectangle(20, 20, 160, 200);
+
+      for (int i = 1; i <= 1000; ++i)
+        this.checkedlistBox.Items.Add(string.Format("Item {0}", i), i % 2 != 0);
+    }
+
+    private CheckedListBox checkedlistBox = new CheckedListBox();
+  }
+}
