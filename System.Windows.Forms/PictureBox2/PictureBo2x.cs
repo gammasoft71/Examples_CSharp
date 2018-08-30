64292c0ac3c46aa4c872c37299e2e93b0536ed45
gammasoft71 <gammasoft71@gmail.com>
2018-08-30 23:09:56 +0200

    add PictureBox2

---
+++ System.Windows.Forms/PictureBox2/PictureBo2x.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace Examples {
+  class MainForm : Form {
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      this.pictureBox1.Parent = this;
+      this.pictureBox1.Size = new System.Drawing.Size(280, 280);
+      this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+      this.pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+      this.pictureBox1.Image = Examples.Properties.Resources.Logo;
+      this.pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+
+      this.Text = "PictureBox example";
+      this.ClientSize = new System.Drawing.Size(300, 300);
+    }
+
+    private PictureBox pictureBox1 = new PictureBox();
+  }
+}
