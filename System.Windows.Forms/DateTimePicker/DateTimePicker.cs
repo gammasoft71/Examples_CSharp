56ab43c7e0213a2e227bda181b648cc15e35abf7
gammasoft71 <gammasoft71@gmail.com>
2018-08-31 08:48:38 +0200

    add DateTimePicker

---
+++ System.Windows.Forms/DateTimePicker/DateTimePicker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examples {
+  class MainForm : Form {
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      this.Text = "DateTimePicker example";
+
+      this.dateTimePicker1.Parent = this;
+      this.dateTimePicker1.Value = new DateTime(1975, 4, 4);
+      this.dateTimePicker1.MinDate = new DateTime(1975, 4, 4);
+      this.dateTimePicker1.MaxDate = DateTime.Now;
+      this.dateTimePicker1.Location = new System.Drawing.Point(30, 30);
+    }
+
+    private DateTimePicker dateTimePicker1 = new DateTimePicker();
+  }
+}
