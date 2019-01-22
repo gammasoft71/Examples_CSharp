fccb74cb361a42850bba0307b6340df82fe5f826
gammasoft71 <gammasoft71@gmail.com>
2019-01-22 20:43:35 +0100

    add nex examples

---
+++ mscorelib/StringSplit/StringSplit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Examples {
+  class MainClass {
+    public static void Main(string[] args) {
+      var strings = "One Two Three Four\nFive".Split();
+
+      Console.WriteLine($"strings.Length = {strings.Length}");
+      foreach (var s in strings)
+        Console.WriteLine(s);
+    }
+  }
+}
+
+// This code produces the following output:
+//
+// strings.Length = 5
+// One
+// Two
+// Three
+// Four
+// Five
--- mscorelib/StringUnicode2/StringUnicode2.cs
+++ mscorelib/StringUnicode2/StringUnicode2.cs
@@ -3,12 +3,27 @@
 namespace Examples {
   class MainClass {
     public static void Main(string[] args) {
+      string str = "\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A\u002C\u0020\u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA\U0001F428\u0021";
+
       Console.OutputEncoding = System.Text.Encoding.UTF8;
-      Console.WriteLine(string.Join(" ", "\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A, \u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA\U0001F428"));
+      Console.WriteLine(string.Join(" ", str));
+
+      // Remarks : the koala character (\U0001F428) is splitted into two utf16...
+      string result = "";
+      foreach (var c in str)
+        result += string.Format("\\u{0:X4}", (int)c);
+      Console.WriteLine(result);
+
     }
   }
 }
 
-// This code produces the following output:
+// This code produces the following output on Windows:
 //
-// 日 本 語 の ひ ら が な ,   漢 字 と カ タ カ ナ ? ?
+// ? ? ? ? ? ? ? ? ,   ? ? ? ? ? ? ? ? !
+// \u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A\u002C\u0020\u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA\uD83D\uDC28\u0021
+
+// Or the following output on utf8 console (macOS or linux):
+//
+// 日 本 語 の ひ ら が な ,   漢 字 と カ タ カ ナ 🐨 !
+// \u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A\u002C\u0020\u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA\uD83D\uDC28\u0021
