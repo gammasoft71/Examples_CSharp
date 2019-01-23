58266f4b952c664c68c7768f38617ef16e940909
gammasoft71 <gammasoft71@gmail.com>
2019-01-23 21:40:10 +0100

    remove unused resource files

--- System.Windows.Forms/Resources/Properties/Resources.Designer.cs
+++ System.Windows.Forms/Resources/Properties/Resources.Designer.cs
@@ -93,16 +93,6 @@ namespace Examples.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Owner {
-            get {
-                object obj = ResourceManager.GetObject("Owner", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] ReadMe {
