27eb0e37e5cb6c6a2a34777470bdc0d94de50702
Gammasoft71 <gammasoft71@gmail.com>
2019-02-03 23:59:23 +0100

    Update TestConsole.cs
    remove unused code

--- Tests/TestConsole/TestConsole.cs
+++ Tests/TestConsole/TestConsole.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-using System.IO;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 
 namespace TestConsole {
   class MainClass {
     public static void Main(string[] args) {
-      //Console.WriteLine("Hello World!");
-      Match match = Regex.Match("bonjour à tous et surtout bonjour", "bonjour$");
-      CaptureCollection captures = match.Captures;
-      foreach (Capture capture in captures)
-        Console.WriteLine("capture = {0}, index {1}, length {2}", capture, capture.Index, capture.Length);
+      Console.WriteLine("Hello World!");
     }
   }
 }
