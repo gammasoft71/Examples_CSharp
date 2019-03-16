587c6b0161bece716226aa5dc69dd047d16e7182
gammasoft71 <gammasoft71@gmail.com>
2019-03-16 23:01:21 +0100

    add TreeView example

---
+++ System.Windows.Forms/TreeView/README.md
@@ -0,0 +1,16 @@
+# TreeView
+
+[This example](.) demonstrates the use of System.Windows.Forms.TreeView control.
+
+# Sources
+
+[TreeView.cs](TreeView.cs)
+
+# Build and run
+
+Open [TreeView.csproj](TreeView.csproj)
+
+# Output
+
+![Screenshot](../../docs/Pictures/Forms/TreeView.png)
+
---
+++ System.Windows.Forms/TreeView/TreeView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examples {
+  class MainForm : Form {
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      this.Text = "TreeView example";
+      this.ClientSize = new System.Drawing.Size(300, 300);
+
+      this.treeView.Parent = this;
+      this.treeView.Location = new System.Drawing.Point(10, 10);
+      this.treeView.Size = new System.Drawing.Size(150, 200);
+      this.treeView.CheckBoxes = true;
+      this.treeView.Nodes.Add("Root");
+      this.treeView.Nodes[0].Nodes.Add("First");
+      this.treeView.Nodes[0].Nodes[0].Nodes.Add("Second");
+      this.treeView.Nodes[0].Nodes[0].Nodes[0].Checked = true;
+      this.treeView.Nodes[0].Nodes[0].Nodes.Add("Third");
+      this.treeView.Nodes[0].Nodes.Add("Fourth");
+      this.treeView.Nodes[0].Nodes[1].Nodes.Add("Fifth");
+      this.treeView.Nodes[0].Nodes[1].Nodes.Add("Sixfth");
+      this.treeView.Nodes[0].Nodes[1].Nodes[1].Checked = true;
+      this.treeView.Nodes[0].Nodes[1].Nodes.Add("Seventh");
+      this.treeView.ExpandAll();
+    }
+
+    private TreeView treeView = new TreeView();
+  }
+}
