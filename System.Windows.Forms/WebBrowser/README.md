1fe28f14860b5e1836c137d6c6bd59e71e5ea311
gammasoft71 <gammasoft71@gmail.com>
2019-03-16 23:45:50 +0100

    add WebBrowser example

---
+++ System.Windows.Forms/WebBrowser/README.md
@@ -0,0 +1,16 @@
+# WebBrowser
+
+[This example](.) demonstrates the use of System.Windows.Forms.WebBrowser control.
+
+# Sources
+
+[WebBrowser.cs](WebBrowser.cs)
+
+# Build and run
+
+Open [WebBrowser.csproj](WebBrowser.csproj)
+
+# Output
+
+![Screenshot](../../docs/Pictures/Forms/WebBrowser.png)
+
---
+++ System.Windows.Forms/WebBrowser/WebBrowser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examples {
+  class MainForm : Form {
+    [STAThread]
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      this.Text = "WebBrowser example";
+      this.ClientSize = new System.Drawing.Size(1024, 768);
+
+      this.webBrowser.Parent = this;
+      this.webBrowser.Dock = DockStyle.Fill;
+      this.webBrowser.Url = new Uri("https://gammasoft71.wixsite.com/gammasoft");
+    }
+
+    private WebBrowser webBrowser = new WebBrowser();
+  }
+}
