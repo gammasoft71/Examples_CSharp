71474cbf8cdb94c24f8274c351aa8adb3d06f765
gammasoft71 <gammasoft71@gmail.com>
2019-03-17 23:08:36 +0100

    add OpenFileDialog example

---
+++ System.Windows.Forms/OpenFileDialog/OpenFileDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FolderBrowserDialogExample {
+  class Form1 : Form {
+    // The main entry point for the application.
+    [STAThread]
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new Form1());
+    }
+
+    public Form1() {
+      this.button.Text = "Open...";
+      this.button.Location = new System.Drawing.Point(10, 10);
+      this.button.Click += delegate(object sender, EventArgs e) {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+          this.label.Text = string.Format("File = {0}", openFileDialog.FileName);
+      };
+
+      this.label.Text = "Path = ";
+      this.label.Location = new System.Drawing.Point(10, 40);
+      this.label.Width = 340;
+
+      this.Text = "OpenFileDialog example";
+      this.Controls.AddRange(new Control[] { this.button, this.label});
+    }
+
+    private Button button = new Button();
+    private Label label = new Label();
+  }
+}
---
+++ System.Windows.Forms/OpenFileDialog/README.md
@@ -0,0 +1,16 @@
+# OpenFileDialog
+
+[This example](.) demonstrates the use of System.Windows.Forms.OpenFileDialog dialog.
+
+# Sources
+
+[OpenFileDialog.cs](OpenFileDialog.cs)
+
+# Build and run
+
+Open [OpenFileDialog.csproj](OpenFileDialog.csproj)
+
+# Output
+
+![Screenshot](../../docs/Pictures/Forms/OpenFileDialog.png)
+
