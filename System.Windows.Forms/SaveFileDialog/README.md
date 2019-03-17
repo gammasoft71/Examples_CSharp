267fc9882ea13998f4771b633f99ac7283744c58
gammasoft71 <gammasoft71@gmail.com>
2019-03-17 23:31:30 +0100

    add SaveFolderDialog example

---
+++ System.Windows.Forms/SaveFileDialog/README.md
@@ -0,0 +1,18 @@
+# SaveFileDialog
+
+[This example](.) demonstrates the use of System.Windows.Forms.SaveFileDialog dialog.
+
+# Sources
+
+[SaveFileDialog.cs](SaveFileDialog.cs)
+
+# Build and run
+
+Open [SaveFileDialog.csproj](SaveFileDialog.csproj)
+
+# Output
+
+![Screenshot](../../docs/Pictures/Forms/SaveFileDialog1.png)
+
+![Screenshot](../../docs/Pictures/Forms/SaveFileDialog2.png)
+
---
+++ System.Windows.Forms/SaveFileDialog/SaveFileDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FolderBrowserDialogExample {
+  class Form1 : Form {
+    // The main entry point for the application.
+    [STAThread]
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new Form1());
+    }
+
+    public Form1() {
+      this.button.Text = "Save...";
+      this.button.Location = new System.Drawing.Point(10, 10);
+      this.button.Click += delegate(object sender, EventArgs e) {
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        saveFileDialog.FileName = "MyFile.txt";
+        saveFileDialog.Filter = "Text Files (*.txt)|*.txt;*.md|All Files (*.*)|*.*";
+        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+          this.label.Text = string.Format("File = {0}", saveFileDialog.FileName);
+      };
+
+      this.label.Text = "Path = ";
+      this.label.Location = new System.Drawing.Point(10, 40);
+      this.label.Width = 340;
+
+      this.Text = "SaveFileDialog example";
+      this.Controls.AddRange(new Control[] { this.button, this.label});
+    }
+
+    private Button button = new Button();
+    private Label label = new Label();
+  }
+}
