2bb73264a80beae608a6cb1a68ca8b140febf8b3
gammasoft71 <gammasoft71@gmail.com>
2019-06-26 14:43:13 +0200

    Update LinkLabel.cs

--- System.Windows.Forms/LinkLabel/LinkLabel.cs
+++ System.Windows.Forms/LinkLabel/LinkLabel.cs
@@ -12,10 +12,11 @@ namespace Examples {
       this.Text = "LinkLabel example";
 
       this.label1.Parent = this;
-      this.label1.Text = "Gammasoft";
+      this.label1.Text = "Gammasoft present CSharp examples";
       this.label1.AutoSize = true;
       this.label1.Location = new System.Drawing.Point(10, 10);
-      this.label1.Links.Add(new LinkLabel.Link(0, this.label1.Text.Length, "https://gammasoft71.wixsite.com/gammasoft"));
+      this.label1.Links.Add(new LinkLabel.Link(0, 9, "https://gammasoft71.wixsite.com/gammasoft"));
+      this.label1.Links.Add(new LinkLabel.Link(18, 6, "https://gammasoft71.wixsite.com/gammasoft/csharp"));
       this.label1.LinkClicked += delegate(object sender, LinkLabelLinkClickedEventArgs e) {
         e.Link.Visited = true;
         System.Diagnostics.Process.Start(e.Link.LinkData as string);
