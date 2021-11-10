0914967b2d6cee4168e22915101fceede0e998d5
Gammasoft <gammasoft71@gmail.com>
2021-11-10 23:13:12 +0100

    add KeyEvents example

---
+++ System.Windows.Forms/KeyEvents/KeyEvents.cs
@@ -0,0 +1,29 @@
+﻿#define TRACE
+
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace KeyEvents {
+  class MainForm : Form {
+    public static void Main() {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+
+    public MainForm() {
+      Text = "Key events example";
+
+      KeyDown += delegate (object sender, KeyEventArgs e) {
+        Trace.WriteLine(string.Format("KeyDown={{KeyCode={0}, KeyData=[{1}], Value=0x{2:X4}, Modifiers=[{3}]}}", e.KeyCode, e.KeyData, e.KeyValue, e.Modifiers));        
+      };
+      KeyPress += delegate (object sender, KeyPressEventArgs e) {
+        Trace.WriteLine(string.Format("KeyPress={{KeyChar={0}}}", e.KeyChar));
+      };
+      KeyUp+= delegate (object sender, KeyEventArgs e) {
+        Trace.WriteLine(string.Format("KeyUp={{KeyCode={0}, KeyData=[{1}], Value=0x{2:X4}, Modifiers=[{3}]}}", e.KeyCode, e.KeyData, e.KeyValue, e.Modifiers));
+      };
+    }
+
+    private Label label = new Label();
+  }
+}
---
+++ System.Windows.Forms/KeyEvents/README.md
@@ -0,0 +1,16 @@
+# HelloWorldForm
+
+[This example](.) illustrate the classic first application ‘Hello World’.
+
+# Sources
+
+[HelloWorldForm.cs](HelloWorldForm.cs)
+
+# Build and run
+
+Open [HelloWorldForm.csproj](HelloWorldForm.csproj)
+
+# Output
+
+![Screenshot](../../docs/Pictures/Forms/HelloWorldForm.png)
+
