059bf9f2ebf10c70ed6728a2f5cc9805a4ab26f2
Gammasoft <gammasoft71@gmail.com>
2022-05-25 13:53:39 +0200

    Update ToolBar example

--- System.Windows.Forms/ToolBar/ToolBar.cs
+++ System.Windows.Forms/ToolBar/ToolBar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Examples {
@@ -11,8 +12,9 @@ namespace Examples {
 
     public MainForm() {
       Text = "ToolBar example";
+      ClientSize = new Size(820, 500);
+      Controls.AddRange(new Control[] {listBox1, toolBar1});
 
-      listBox1.Parent = this;
       listBox1.Dock = DockStyle.Fill;
 
       newToolBarbutton.Text = "&New";
@@ -23,29 +25,51 @@ namespace Examples {
 
       saveToolBarbutton.Text = "&Save";
       saveToolBarbutton.ImageIndex = 2;
-
+      saveToolBarbutton.Style = ToolBarButtonStyle.ToggleButton;
+      
       separatorToolBarbutton1.Style = System.Windows.Forms.ToolBarButtonStyle.Separator;
 
-      aboutToolBarbutton.Text = "&About";
-      aboutToolBarbutton.ImageIndex = 3;
+      dropDownMenu1.MenuItems.AddRange(new MenuItem[] {
+        new MenuItem("&Content", OnMenuItemClick),
+        new MenuItem("&Index", OnMenuItemClick),
+        new MenuItem("&Search", OnMenuItemClick),
+        new MenuItem("-"),
+        new MenuItem("&About", OnMenuItemClick),
+      });
 
-      toolBar1.Parent = this;
-      toolBar1.Buttons.AddRange(new ToolBarButton[] {newToolBarbutton, openToolBarbutton, saveToolBarbutton, separatorToolBarbutton1, aboutToolBarbutton});
-      toolBar1.ButtonClick += ToolBar1_ButtonClick;
+      helpToolBarbutton.Text = "&Help";
+      helpToolBarbutton.ImageIndex = 3;
+      helpToolBarbutton.DropDownMenu = dropDownMenu1;
+      helpToolBarbutton.Style = ToolBarButtonStyle.DropDownButton;
 
+      toolBar1.ImageList = new ImageList {ColorDepth = ColorDepth.Depth32Bit};
+      toolBar1.ImageList.Images.AddRange(new Image[] { ToolBarResource.New, ToolBarResource.Open, ToolBarResource.Save, ToolBarResource.About });
+      toolBar1.Buttons.AddRange(new ToolBarButton[] { newToolBarbutton, openToolBarbutton, saveToolBarbutton, separatorToolBarbutton1, helpToolBarbutton });
+      toolBar1.ButtonClick += OnToolBarButtonClick;
+      toolBar1.TextAlign = ToolBarTextAlign.Right;
     }
 
-    private void ToolBar1_ButtonClick(object sender, ToolBarButtonClickEventArgs e) {
-      listBox1.Items.Add(string.Format("Item {0} clicked", e.Button.Text));
+    private void OnToolBarButtonClick(object sender, ToolBarButtonClickEventArgs e) {
+      if (e.Button.Style == ToolBarButtonStyle.ToggleButton)
+        listBox1.Items.Add(string.Format("Button {0} clicked, pushed = {1}", e.Button.Text, e.Button.Pushed));
+      else
+        listBox1.Items.Add(string.Format("Button {0} clicked", e.Button.Text));
       listBox1.SelectedIndex = listBox1.Items.Count - 1;
     }
 
-    private ToolBar toolBar1 = new ToolBar();
+    private void OnMenuItemClick(object sender, EventArgs e) {
+      listBox1.Items.Add(string.Format("{0} clicked", (sender as MenuItem).Text));
+      listBox1.SelectedIndex = listBox1.Items.Count - 1;
+    }
+
     private ListBox listBox1 = new ListBox();
-    private ToolBarButton newToolBarbutton= new ToolBarButton();
+
+    private ToolBar toolBar1 = new ToolBar();
+    private ToolBarButton newToolBarbutton = new ToolBarButton();
     private ToolBarButton openToolBarbutton = new ToolBarButton();
     private ToolBarButton saveToolBarbutton = new ToolBarButton();
     private ToolBarButton separatorToolBarbutton1 = new ToolBarButton();
-    private ToolBarButton aboutToolBarbutton = new ToolBarButton();
+    private ToolBarButton helpToolBarbutton = new ToolBarButton();
+    private ContextMenu dropDownMenu1 = new ContextMenu();
   }
 }
