f142c84b482a72c36f00942e796a5a6166e8a5b6
Gammasoft <gammasoft71@gmail.com>
2022-05-25 13:52:29 +0200

    Update TestConsole.cs

--- Tests/TestConsole/TestConsole.cs
+++ Tests/TestConsole/TestConsole.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using System.Net;
 using System.Drawing;
 
 namespace TestConsole {
   class MainClass {
     public static void Main(string[] args) {
-      SystemIcons.Application.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Application.ico")));
-      SystemIcons.Error.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Error.ico")));
-      SystemIcons.Exclamation.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Exclamation.ico")));
-      SystemIcons.Information.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Information.ico")));
-      SystemIcons.Question.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Question.ico")));
-      SystemIcons.Shield.Save(System.IO.File.OpenWrite(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Shield.ico")));
+      Console.WriteLine("Version = {0}", Environment.OSVersion);
+      Console.WriteLine("Version = {0}", System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion());
+      Console.WriteLine("Version = {0}", System.Runtime.InteropServices.RuntimeInformation.OSDescription);
+
+      Console.ReadKey();
     }
   }
 }
