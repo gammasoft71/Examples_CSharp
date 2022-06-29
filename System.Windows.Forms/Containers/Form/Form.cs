427194507e19c7df7f5afb1072d7a025a042567a
Gammasoft <gammasoft71@gmail.com>
2022-06-29 12:43:49 +0200

    Add ExitThread

--- System.Windows.Forms/Containers/Form/Form.cs
+++ System.Windows.Forms/Containers/Form/Form.cs
@@ -19,6 +19,12 @@ namespace Examples {
       buttonExit.Text = "Exit";
       buttonExit.Location = new System.Drawing.Point(100, 10);
       buttonExit.Click += delegate (object sender, EventArgs e) {
+        Application.Exit();
+      };
+
+      buttonExitThread.Text = "Exit Thread";
+      buttonExitThread.Location = new System.Drawing.Point(190, 10);
+      buttonExitThread.Click += delegate (object sender, EventArgs e) {
         Application.ExitThread();
       };
 
@@ -26,8 +32,7 @@ namespace Examples {
       StartPosition = FormStartPosition.Manual;
       Location = new System.Drawing.Point(300, 200);
       Size = new System.Drawing.Size(640, 480);
-      Controls.Add(buttonClose);
-      Controls.Add(buttonExit);
+      Controls.AddRange(new Control[] {buttonClose, buttonExit, buttonExitThread});
       FormClosing += delegate(object sender, FormClosingEventArgs e) {
         e.Cancel = MessageBox.Show("Are you sure you want exit?", "Close Form", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No;
       };
@@ -35,5 +40,6 @@ namespace Examples {
 
     private Button buttonClose = new Button();
     private Button buttonExit = new Button();
+    private Button buttonExitThread = new Button();
   }
 }
