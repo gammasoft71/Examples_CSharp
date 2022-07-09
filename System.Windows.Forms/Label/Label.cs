2c6dd8d1c4efbfd35c154b029991343acca4d173
Gammasoft <gammasoft71@gmail.com>
2022-07-09 23:40:47 +0200

    Update Label.cs

--- System.Windows.Forms/Label/Label.cs
+++ System.Windows.Forms/Label/Label.cs
@@ -10,12 +10,31 @@ namespace Examples {
 
     public MainForm() {
       Text = "Label example";
+      ClientSize = new System.Drawing.Size(400, 400);
 
       label1.Parent = this;
-      label1.Text = "label1";
-      label1.Location = new System.Drawing.Point(10, 10);
+      label1.Text = "Lorem ipsum";
+      label1.Location = new System.Drawing.Point(10, 50);
+
+      label2.Parent = this;
+      label2.AutoSize = true;
+      label2.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+      label2.Location = new System.Drawing.Point(10,100);
+
+      label3.Parent = this;
+      label3.AutoEllipsis = true;
+      label3.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+      label3.Location = new System.Drawing.Point(10, 150);
+
+      label4.Parent = this;
+      label4.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed non risus.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.Pellentesque congue. Ut in risus volutpat libero pharetra tempor.Cras vestibulum bibendum augue. Praesent egestas leo in pede.Praesent blandit odio eu enim.Pellentesque sed dui ut augue blandit sodales.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum.Maecenas adipiscing ante non diam sodales hendrerit.\n\nUt velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar.Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula.Pellentesque rhoncus nunc et augue.Integer id felis.Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Morbi vel erat non mauris convallis vehicula.Nulla et sapien.Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat.Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.\n\nAliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus.Fusce vulputate sem at sapien.Vivamus leo. Aliquam euismod libero eu enim.Nulla nec felis sed leo placerat imperdiet.Aenean suscipit nulla in justo.Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus.Curabitur eu amet.\n";
+      label4.Location = new System.Drawing.Point(10, 200);
+      label4.Size = new System.Drawing.Size(300, 150);
     }
 
     private Label label1 = new Label();
+    private Label label2 = new Label();
+    private Label label3 = new Label();
+    private Label label4 = new Label();
   }
 }
